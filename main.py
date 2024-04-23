e552b7f787c9b47b5c07867575df0712d0ff2baf
Ramil <tlsmscience@gmail.com>
2024-04-23 12:33:54 +0500

    Сохраняем мастер

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import tryangle as tr
+
 def main():
-    pass
+    tryangle = tr.Tryangle(int(input('Enter a tryangle number: ')))
+    tryangle.generate_tryangle()
+    print(tryangle.result())
 
 if __name__ == '__main__':
     main()
