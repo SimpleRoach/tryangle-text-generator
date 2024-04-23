0793c7f85ba59661890efa673a1b484f611e7827
Ramil <tlsmscience@gmail.com>
2024-04-23 12:17:39 +0500

    файл треугольника

---
+++ tryangle.py
@@ -0,0 +1,17 @@
+class Tryangle:
+    def __init__(self, size):
+        self.tryangle = []
+        self.string = ""
+        self.size = size
+
+    def generate_tryangle(self):
+        for i in range(self.size):
+            self.tryangle.append(i)
+        return self.tryangle
+
+    def result (self):
+        for i in range self.tryangle:
+            for j in range (self.tryangle[:i+1]):
+                self.string += str(j) + "\t"
+            self.string += "\n"
+        return self.string
